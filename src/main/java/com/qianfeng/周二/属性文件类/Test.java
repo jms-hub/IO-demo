5503ccb8223a01033ff4ea37f26c6ba7815b584e
23737 <237377116@qq.com>
2020-02-11 16:10:41 +0800

    今天我写了 课程的代码，请大家下载

--- src/main/java/com/qianfeng/周二/属性文件类/Test.java
+++ src/main/java/com/qianfeng/周二/属性文件类/Test.java
@@ -7,12 +7,8 @@ import java.util.Properties;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-
         Properties properties = System.getProperties();// 系统文件属性类
-
-
         System.err.println(properties);
         // os.name判断你当前的操作系统，在web阶段，会用来判断是线上还是开发环境，用来设置文件的映射路径
-
     }
 }
