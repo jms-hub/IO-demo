7f4e5cd87d350ef1668d25e6a7a233fdf1cdd851
23737 <237377116@qq.com>
2020-02-12 00:06:18 +0800

    ··

--- src/main/java/com/qianfeng/wednesday/Test.java
+++
@@ -1,9 +0,0 @@
-package com.qianfeng.wednesday;
-
-public class Test {
-    public static void main(String[] args) {
-        MyThread myThread = new MyThread();
-        myThread.setName("线程-张三");
-        myThread.start();
-    }
-}
