b8844de39f4a245fb2c7d852463dedbde7e02caf
23737 <237377116@qq.com>
2020-02-12 17:54:15 +0800

    纠正第三种创建线程的方式哈

--- src/main/java/com/qianfeng/wednesday/Test2.java
+++ src/main/java/com/qianfeng/wednesday/Test2.java
@@ -12,6 +12,8 @@ public class Test2 {
             public String call() throws Exception {
                 for (int x = 0; x < 101; x++) {
                     System.err.println("迅雷便下载边看" + x + "%");
+
+                    System.err.println(Thread.currentThread().getName());
                     Thread.sleep(50);// 沉睡0.1秒
                 }
                 return "call线程已经运行完毕，请老大发号司令！";// call方法运行完毕之后，返回值
@@ -19,8 +21,12 @@ public class Test2 {
         };
         FutureTask<String> futureTask = new FutureTask(callable);
 //        futureTask.cancel(true);// 取消线程
-        futureTask.run();// 开启线程，让线程处于就绪状态！
-//        futureTask.cancel(true);// 取消线程
+//        futureTask.run();// 开启线程，让线程处于就绪状态！
+       Thread thread1= new Thread(futureTask);// 开启新线程
+       thread1.start();
+       Thread.sleep(3000);
+        futureTask.cancel(true);// 取消线程
+
         String res = futureTask.get();// 获取线程执行完毕之后的返回值，一直会等到结果出来，才会离开，才会继续往后执行
         // 思考：假如我现在要取消这个线程，能否再get()方法后面设置？
         System.err.println("线程运行完毕之后的返回值是：" + res);// 也就是说FutureTask是包含了线程运行完毕的一个状态
